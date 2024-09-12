a8f4d67a53aee776e231a6a6836bcf12cbe35617
WeslleyAndrik <146298079+WeslleyAndrik@users.noreply.github.com>
2024-09-12 16:58:14 -0300

    Add files via upload

---
+++ Teste Target Sistemas/tecnica5.py
@@ -0,0 +1,9 @@
+def inverter_string(s):
+    invertida = ''
+    for char in s:
+        invertida = char + invertida
+    return invertida
+
+# Entrada da string
+string = input("Informe uma string: ")
+print(f"String invertida: {inverter_string(string)}")
